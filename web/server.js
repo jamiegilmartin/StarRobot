11aad4a4526fa2967cfc7969f8ec6e29d4805932
jamiegilmartin <jamiegilmartin@gmail.com>
2013-07-08 14:23:09 -0400

    move ui into web folder

--- web/server.js
+++ web/server.js
@@ -1,12 +1,12 @@
 
 var app = require('http').createServer(handler),
-    fs = require('fs'),
-    serialport = require("serialport"),
-    SerialPort = serialport.SerialPort,
-    serial = new SerialPort("/dev/tty.usbmodem3d21", {
-        baudrate: 9600,
-        parser: serialport.parsers.readline("\n") 
-    });
+	fs = require('fs'),
+	serialport = require("serialport"),
+	SerialPort = serialport.SerialPort,
+	serial = new SerialPort("/dev/tty.usbmodem3d21", {
+		baudrate: 9600,
+		parser: serialport.parsers.readline("\n") 
+	});
 
 app.listen(8080);
 console.log("Listening on http://raspberrypi:8080...");
@@ -16,17 +16,17 @@ serial.write("hellz!")
 // directs page requests to html files
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
+	fs.readFile(__dirname + '/ui/index.html',
+	function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
 serial.on("data", function (data) {
-    console.log("from arduino: "+data);
+	console.log("from arduino: "+data);
 });
