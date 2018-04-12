9fd87969a098918bdb1b7c399d7109ffa56f1ca2
jamie gilmartin <jamiegilmartin@gmail.com>
2018-04-12 22:22:18 +0200

    try new python sonic

--- python/sonic.py
+++ python/sonic.py
@@ -5,27 +5,30 @@ GPIO.setmode(GPIO.BCM)
 TRIG = 26
 ECHO = 24
 
+#set GPIO direction (IN / OUT)
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
-GPIO.output(TRIG, False)
-time.sleep(2)
-
+# set Trigger to HIGH
 GPIO.output(TRIG, True)
+# set Trigger after 0.01ms to LOW
 time.sleep(0.00001)
 GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+StartTime = time.time()
+StopTime = time.time()
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+while GPIO.input(ECHO) == 0:
+  StartTime = time.time()
+# save time of arrival
+while GPIO.input(ECHO) == 1:
+  StopTime = time.time()
 
-pulse_duration = pulse_end - pulse_start
-
-distance = pulse_duration * 17150
-
-distance = round(distance, 2)
+# time difference between start and arrival
+TimeElapsed = StopTime - StartTime
+# multiply with the sonic speed (34300 cm/s)
+# and divide by 2, because there and back
+distance = (TimeElapsed * 34300) / 2
 
 print "distance",distance,"cm"
 
